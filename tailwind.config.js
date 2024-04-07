742671abb92bfce5b60e4d0df4d19bf8a36f8249
Ankita <ankitabxr02@gmail.com>
2024-04-07 18:50:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
